7f925e3137c11405c6290fb47300700a94c98a20
Raist Qian <raistlinqjx@gmail.com>
2016-12-03 10:57:27 +0800

    Initial Commit

---
+++ RQTripleTriadList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RQTripleTriadList
+//
+//  Created by Qian Jixiang on 2016/12/3.
+//  Copyright © 2016年 Qian Jixiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RQTripleTriadList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RQTripleTriadList
+//
+//  Created by Qian Jixiang on 2016/12/3.
+//  Copyright © 2016年 Qian Jixiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
